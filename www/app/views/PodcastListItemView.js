f06ea8d053ebdaca2f6c84d1558e19de83e46bed
Mike Rogers <mike.r@fullondesign.co.uk>
2013-08-16 13:31:19 +0100

    convering images to resize via canvas

--- www/app/views/PodcastListItemView.js
+++ www/app/views/PodcastListItemView.js
@@ -23,10 +23,36 @@ PodcastListItemView = Backbone.View.extend({
 
         this.$el.html(template);
 
+        this.addThumbnail();
+
         // Returning the object is a good practice
         // that makes chaining possible
         return this;
-    }
+    },
 
-    
+    addThumbnail: function(){
+        var aLink = this.$el.find('a'),
+        img = new Image();
+
+        // Load the thumbnail
+        img.onload = function(e){
+            var copyCanvas = document.createElement("canvas"),
+            copyCtx = copyCanvas.getContext("2d"),
+            canvas = document.createElement("canvas"),
+            ctx = canvas.getContext("2d");
+
+            canvas.width = 120;
+            canvas.height= 120;
+
+            copyCanvas.width = this.width;
+            copyCanvas.height = this.height;
+            copyCtx.drawImage(img,0,0);
+
+            ctx.drawImage(copyCanvas, 0, 0, 120, 120);
+
+            aLink.append(canvas);
+        }
+
+        img.src = this.model.getImageURI();
+    }
 });
